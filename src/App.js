f1ac3798585b120165ab84e64d8d2417e7fc3f53
dayong <761385718@qq.com>
2019-04-03 10:53:03 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+/*应用跟组件
+*
+* */
+
+
+import React, {Component} from 'react'
+import {Button} from 'antd'
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Button type='danger'>测试Button1</Button>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
